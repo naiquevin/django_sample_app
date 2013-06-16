10ee7b59d0fe78e5f450e0ac6b566bacf38b9056
Vineet <naikvin@gmail.com>
2013-06-16 14:07:18 +0530

    Add DB_NAME to localsettings

--- polls/settings.py
+++ polls/settings.py
@@ -1,5 +1,7 @@
 # Django settings for polls project.
 
+import localsettings
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/vineet/python/djangoCodeJam/django_sample_app/polls.db',                      # Or path to database file if using sqlite3.
+        'NAME': localsettings.DB_NAME,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
